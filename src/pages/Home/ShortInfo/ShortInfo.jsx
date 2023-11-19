bfe58786534088076506b924d8dd3e35bd0c1fc9
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-11-19 20:24:59 +0600

    Short Info section added

--- src/pages/Home/ShortInfo/ShortInfo.jsx
+++ src/pages/Home/ShortInfo/ShortInfo.jsx
@@ -7,9 +7,9 @@ const ShortInfo = () => {
     >
       <img className="w-full lg:h-[460px] xl:h-[480px] 2xl:h-[550px]" src="https://i.ibb.co/7gnrFRr/short-info.png" alt="Short Info Background" />
       <div className="absolute text-center px-4 py-2 md:px-5 md:py-3 lg:py-10 xl:py-14 2xl:py-18 lg:px-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-600 dark:bg-slate-900 text-white">
-        <h2 className="uppercase text-xs md:text-2xl font-serif">
-          We're looking forward to hearing from you!
-        </h2>
+        <h2 className="normal-case text-xs md:text-2xl font-serif">
+          We're Looking Forward to Hearing From You!
+        </h2><br></br>
         <p className="text-[7px] md:text-sm">
           We cherish every interaction and love to hear great initiatives. Share
           your ideas with us today and we'll get back to you as soon as possible
