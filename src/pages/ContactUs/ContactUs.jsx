aa629ab6ebda0749a7ab97c62e853a28a42c4f21
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-11-20 17:41:31 +0600

    Enabled send email functionality using Email.js

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Helmet } from "react-helmet-async";
+import toast from "react-hot-toast";
 import "./ContactUs.css";
 
 const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_xa27017",
+        "template_xmlqfzu",
+        form.current,
+        "EZqoPl1QBq6NL4AbD"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast.success("Your Message Has Been Sent Successfully!");
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="bg-indigo-700 px-2 py-3 lg:px-8 dark:bg-slate-900 dark:text-white">
       {/* Dynamic page title */}
@@ -12,38 +37,50 @@ const ContactUs = () => {
       <div className="grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 justify-center items-center">
         {/*  Text Container*/}
         <div className="lg:col-span-2 xl:col-span-3 mx-3 md:mx-20 lg:mx-5 space-y-3">
-          <p className="text-xl md:text-2xl xl:text-3xl text-white font-semibold">Contact Us</p>
-          <p className="font-bold text-3xl md:text-5xl xl:text-7xl text-white">Let’s collaborate <br /> to bring your <br /> vision to life!</p>
+          <p className="text-xl md:text-2xl xl:text-3xl text-white font-semibold">
+            Contact Us
+          </p>
+          <p className="font-bold text-3xl md:text-5xl xl:text-7xl text-white">
+            Let’s collaborate <br /> to bring your <br /> vision to life!
+          </p>
           <p className="text-[13px] md:text-xl lg:text-base xl:text-2xl text-white">
-            Share your idea! Our team will reach out to discuss it and <br /> provide
-            you with an estimate.
+            Share your idea! Our team will reach out to discuss it and <br />{" "}
+            provide you with an estimate.
           </p>
         </div>
         {/* Form Container */}
-        <div className="lg:col-span-2 xl:col-span-2 dark:bg-slate-800 dark:text-white bg-indigo-600 flex flex-col justify-center items-center gap-2 md:gap-3 rounded-lg mx-3 md:mx-20 lg:mx-5 py-4 md:py-8 mt-6 mb-3">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="lg:col-span-2 xl:col-span-2 dark:bg-slate-800 dark:text-white bg-indigo-600 flex flex-col justify-center items-center gap-2 md:gap-3 rounded-lg mx-3 md:mx-20 lg:mx-5 py-4 md:py-8 mt-6 mb-3"
+        >
           <input
             placeholder="Full Name"
             className="px-4 py-2 md:py-3 rounded-3xl border-2 border-blue-700 outline-none w-11/12 md:w-10/12"
             type="text"
+            name="user_name"
           />
           <input
             placeholder="Email Address"
             className="px-4 py-2 md:py-3 rounded-3xl border-2 border-blue-700 w-11/12 md:w-10/12 outline-none"
-            type="text"
+            type="email"
+            name="user_email"
           />
           <input
             placeholder="Phone Number"
             className="px-4 py-2 md:py-3 rounded-3xl border-2 border-blue-700 w-11/12 md:w-10/12 outline-none"
-            type="text"
+            type="number"
+            name="number"
           />
           <input
             placeholder="Subject"
             className="px-4 py-2 md:py-3 rounded-3xl border-2 border-blue-700 w-11/12 md:w-10/12 outline-none"
             type="text"
+            name="subject"
           />
           <textarea
             className="px-4 py-2 md:py-3 rounded-3xl border-2 border-blue-700 w-11/12 md:w-10/12 outline-none"
-            name="Send Message"
+            name="message"
             cols="20"
             rows="4"
             placeholder="How can we help?"
@@ -51,10 +88,11 @@ const ContactUs = () => {
           <div className="">
             <input
               className="bg-orange-600 text-base md:text-lg py-[10px] cursor-pointer text-white mt-1 rounded-full px-4 md:px-6 w-full"
-              type="Submit"
+              type="submit"
+              value="Send"
             />
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
